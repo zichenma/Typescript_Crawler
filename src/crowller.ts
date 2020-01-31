2bebb3c7ba62cc3e54091aa357a8f89d51ae84db
zichen ma <zma@fundopolis.com>
2020-01-31 12:11:26 -0500

    Finished 3_4

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,11 +1,46 @@
+import fs from 'fs';
+import path from 'path';
 // ts -> .d.ts 翻译文件 -> js
-
 import superagent from 'superagent';
 import cheerio from 'cheerio';
 
 interface Course {
   title: string;
   count: number;
+}
+
+interface CourseResult {
+  time: number; // type is number
+  data: Course[];
+}
+
+interface Content {
+  // 转化数据结构从：
+  // {
+  //   time: 1580489013468,
+  //   data: [
+  //     { title: 'Vue2.5开发去哪儿网App', count: 74 },
+  //     { title: 'React 16.4 开发简书项目', count: 77 },
+  //     { title: 'React服务器渲染原理解析与实践', count: 94 },
+  //     { title: '手把手带你掌握新版Webpack4.0', count: 55 }
+  //   ]
+  // }
+  // 转化成：
+  // {
+  //   1580489013468 :  [
+  //     { title: 'Vue2.5开发去哪儿网App', count: 74 },
+  //     { title: 'React 16.4 开发简书项目', count: 77 },
+  //     { title: 'React服务器渲染原理解析与实践', count: 94 },
+  //     { title: '手把手带你掌握新版Webpack4.0', count: 55 }
+  //   ],
+  //   1580489013468 :  [
+  //     { title: 'Vue2.5开发去哪儿网App', count: 74 },
+  //     { title: 'React 16.4 开发简书项目', count: 77 },
+  //     { title: 'React服务器渲染原理解析与实践', count: 94 },
+  //     { title: '手把手带你掌握新版Webpack4.0', count: 55 }
+  //   ]
+  // }
+  [propName: number]: Course[];
 }
 
 class Crowller {
@@ -34,21 +69,49 @@ class Crowller {
       );
       courseInfos.push({ title, count });
     });
-    const result = {
+    return {
       time: new Date().getTime(),
       data: courseInfos
     };
-    console.log(result);
   }
 
   async getRawHtml() {
     const result = await superagent.get(this.url);
+    // 因为 getCourseInfo 和 getRawHtml 耦合
+    // 所以需要 initSpiderProcess() 单独处理爬虫功能
+    // 把 getCourseInfo() 放到 initSpiderProcess 里
+    return result.text;
+  }
 
-    this.getCourseInfo(result.text);
+  generateJsonContent(courseInfo: CourseResult) {
+    // 根据当前路径 __dirname 去找跟目录下的 course.json
+    const filePath = path.resolve(__dirname, '../data/course.json');
+    let fileContent: Content = {};
+    // 判断文件是否存在
+    if (fs.existsSync(filePath)) {
+      // 如果有内容则把读取内容存(老数据）在 fileContent
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    }
+    // 把新爬到的内容(新数据)存在 fileContent
+    fileContent[courseInfo.time] = courseInfo.data;
+    // generateJsonContent 只应当负责生成JSON， 所以存数据应当新定义函数完成
+    // fs.writeFileSync(filePath, JSON.stringify(fileContent));
+    return fileContent;
+  }
+
+  async initSpiderProcess() {
+    // 1. 首先获取 HTML
+    // 2. 再拿到课程信息
+    // 3. 存储到Json文件里
+    const html = await this.getRawHtml();
+    const courseInfo = this.getCourseInfo(html);
+    const fileContent = this.generateJsonContent(courseInfo);
+    const filePath = path.resolve(__dirname, '../data/course.json');
+    fs.writeFileSync(filePath, JSON.stringify(fileContent));
   }
 
   constructor() {
-    this.getRawHtml();
+    this.initSpiderProcess();
   }
 }
 

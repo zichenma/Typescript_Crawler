f7b8e77539e284c9d62de8819183f905a20f3483
zichenma <zma@liaisionedu.com>
2020-03-09 09:13:04 -0400

    finished 8_6

--- src/controller/CrollwerController.ts
+++ src/controller/CrollwerController.ts
@@ -24,7 +24,7 @@ const test = (req: Request, res: Response, next: NextFunction): void => {
   next();
 };
 
-@controller('/')
+@controller('/api')
 export class CrowllerController {
   @get('/getData')
   @use(checkLogin)

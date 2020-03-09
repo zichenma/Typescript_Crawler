7171af48d595ee038a9876d6414720a31c8eca48
zichenma <mazichen1984@gmail.com>
2020-03-09 00:21:33 -0400

    7 6 add mutil decrators on func (#2)
    * Finished 3_3
    
    * 3_3 Finished
    
    * Finished 3_4
    
    * Finished 3_5
    
    * 3_5 Finished
    
    * add package.json
    
    * finished 3_6
    
    * finished 3_7
    
    * added remove comments in config
    
    * finished 5_1
    
    * finished 5_1
    
    * finished 5_3
    
    * finished  5_4
    
    * finished 5_5
    
    * finished 7_1
    
    * finsihed 7_2
    
    * 7_3_finished
    
    * finished 7_4
    
    * finished 7_5
    
    * finished  7_6
    
    * added isLogin route from FE
    
    * finished 8_5
    
    Co-authored-by: zichen ma <zma@fundopolis.com>
    Co-authored-by: zichenma <zma@liaisionedu.com>

--- src/controller/LoginController.ts
+++ src/controller/LoginController.ts
@@ -23,14 +23,14 @@ interface BodyRequest extends Request {
 //   }
 // }
 
-@controller('/') // 类的装饰器一般做逻辑的融合
+@controller('/api') // 类的装饰器一般做逻辑的融合
 export class LoginController {
   // 用静态的方法，就可以保证，LoginController 不需要实例化就可以用 isLogin
   static isLogin(req: BodyRequest): boolean {
     return !!(req.session ? req.session.login : false);
   }
   
-  @get('/api/isLogin')
+  @get('/isLogin')
   isLogin(req: BodyRequest, res: Response): void {
     const isLogin = LoginController.isLogin(req);
     res.json(getResponseData(isLogin));
@@ -42,7 +42,7 @@ export class LoginController {
     const isLogin = LoginController.isLogin(req);
 
     if (isLogin) {
-      res.json(getResponseData(false, 'Already login'));
+      res.json(getResponseData(true));
     } else {
       if (password === '123' && req.session) {
         req.session.login = true;
@@ -59,32 +59,5 @@ export class LoginController {
       req.session.login = undefined;
     }
     res.json(getResponseData(true));
-  }
-
-  @get('/')
-  home(req: BodyRequest, res: Response): void {
-    const isLogin = LoginController.isLogin(req);
-    if (isLogin) {
-      res.send(`
-      <html>
-        <body>
-          <a href='/getData'>爬取内容</a>
-          <a href='/showData'>展示内容</a>
-          <a href='/logout'>退出</a>
-        </body>
-      </html>
-    `);
-    } else {
-      res.send(`
-      <html>
-        <body>
-          <form method="post" action="/login">
-            <input type="password" name="password" />
-            <button>登陆</button>
-          </form>
-        </body>
-      </html>
-    `);
-    }
   }
 }
